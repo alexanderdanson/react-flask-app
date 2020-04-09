a6868221366d23c219b24133f89481f568d30062
Alexander Danson <alex.danson2@gmail.com>
2020-04-09 12:49:03 +0200

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
 
@@ -15,6 +19,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous"
+        />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -29,7 +39,7 @@ function App() {
         </a>
 
         <p>The current time is {currentTime}.</p>
-
+        <Button>Test</Button>
       </header>
     </div>
   );
